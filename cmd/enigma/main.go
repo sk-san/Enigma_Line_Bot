f7d74ce7f4a830a13401b1948a660731ae6e103e
sk-san <sho1234vv@gmail.com>
2024-06-21 01:56:57 +0200

    update main.go

--- cmd/enigma/main.go
+++ cmd/enigma/main.go
@@ -1,42 +1,37 @@
 package main
 
 import (
+	"github.com/line/line-bot-sdk-go/linebot"
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 func main() {
-	bot, err := linebot.New(os.Getenv("LINE_BOT_CHANNEL_SECRET"), os.Getenv("LINE_BOT_CHANNEL_TOKEN"))
+	bot, err := linebot.New(
+		os.Getenv("CHANNEL_SECRET"),
+		os.Getenv("CHANNEL_TOKEN"),
+	)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	http.HandleFunc("/callback", func(w http.ResponseWriter, req *http.Request) {
+	http.HandleFunc("/webhook", func(w http.ResponseWriter, req *http.Request) {
 		events, err := bot.ParseRequest(req)
 		if err != nil {
 			if err == linebot.ErrInvalidSignature {
-				w.WriteHeader(http.StatusBadRequest)
+				w.WriteHeader(400)
 			} else {
-				w.WriteHeader(http.StatusInternalServerError)
+				w.WriteHeader(500)
 			}
 			return
 		}
-
 		for _, event := range events {
 			if event.Type == linebot.EventTypeMessage {
 				switch message := event.Message.(type) {
 				case *linebot.TextMessage:
-					if message.Text == "選択肢を見せて" {
-						if _, err = bot.ReplyMessage(event.ReplyToken, createTemplateMessage()).Do(); err != nil {
-							log.Print(err)
-						}
-					} else {
-						if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("「選択肢を見せて」と入力してください")).Do(); err != nil {
-							log.Print(err)
-						}
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+						log.Print(err)
 					}
 				}
 			}
@@ -49,14 +44,4 @@ func main() {
 	}
 
 	log.Fatal(http.ListenAndServe(":"+port, nil))
-}
-
-func createTemplateMessage() *linebot.TemplateMessage {
-	buttons := linebot.NewButtonsTemplate(
-		"", "選択してください", "どれにしますか？",
-		linebot.NewMessageAction("選択肢1", "選択肢1が選ばれました"),
-		linebot.NewMessageAction("選択肢2", "選択肢2が選ばれました"),
-		linebot.NewMessageAction("選択肢3", "選択肢3が選ばれました"),
-	)
-	return linebot.NewTemplateMessage("これはテンプレートメッセージです", buttons)
 }
